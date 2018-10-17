1bba1b46c5bf134518fa00f5c9605da39575295b
Josh King <jking@chambana.net>
2018-10-17 14:58:23 -0400

    Templatize uwsgi configuration file.

--- bin/run.sh
+++ bin/run.sh
@@ -15,8 +15,12 @@ if [[ ! -d $DIR ]]; then
 		https://sourceforge.net/projects/podcastgen/files/podcastgen/podcastgen-${PODCASTGEN_VERSION}/podcastgen-${PODCASTGEN_VERSION}.zip/download
 	[[ $? -eq 0 ]] || { ERR "Failed to download podcastgen, aborting."; exit 1; }
 	unzip -o -d /var/www /tmp/podcastgen.zip
+	[[ $? -eq 0 ]] || { ERR "Failed to unzip podcastgen, perhaps file is invalid?"; exit 1; }
 	[[ -d $DIR ]] || { ERR "Directory $DIR does not exist, aborting."; exit 1; }
 	chown -R www-data:www-data /var/www/
+	sed -i -e "s/^php-docroot\ *=.*/php-docroot\ =\ ${DIR}/" \
+		-e "s/^static-safe\ *=\ {{\ PODCASTGEN_DIR\ }}/static-safe\ =\ ${DIR}/" \
+		/etc/uwsgi/apps-available/podcastgen.conf
 fi
 
 MSG "Serving site..."
