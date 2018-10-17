620c8275d5c08497b875f92b224a48bc0a166702
Josh King <jking@chambana.net>
2018-10-17 14:00:38 -0400

    Remove -f flag from unzip command.

--- bin/run.sh
+++ bin/run.sh
@@ -14,7 +14,7 @@ if [[ ! -d $DIR ]]; then
 	wget -O /tmp/podcastgen.zip \
 		https://sourceforge.net/projects/podcastgen/files/podcastgen/podcastgen-${PODCASTGEN_VERSION}/podcastgen-${PODCASTGEN_VERSION}.zip/download
 	[[ $? -eq 0 ]] || { ERR "Failed to download podcastgen, aborting."; exit 1; }
-	unzip -o -f -d /var/www /tmp/podcastgen.zip
+	unzip -o -d /var/www /tmp/podcastgen.zip
 	[[ -d $DIR ]] || { ERR "Directory $DIR does not exist, aborting."; exit 1; }
 	chown -R www-data:www-data /var/www/
 fi
